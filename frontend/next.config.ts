43d1fda67bf771f02391bb002646e88135d188b5
SatyamDevGenie <satyamsawant54@gmail.com>
2025-02-28 00:53:39 +0530

    Changes - next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+          : "http://localhost:5000/api/:path*",
+      },
+    ];
+  },
+  env: {
+    API_URL: process.env.NEXT_API_URL || "http://localhost:5000",
+  },
 };
 
 export default nextConfig;

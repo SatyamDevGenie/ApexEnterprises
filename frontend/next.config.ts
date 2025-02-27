42f1a5ef68b93f310679c5cc3f853a74c9206dea
SatyamDevGenie <satyamsawant54@gmail.com>
2025-02-28 01:16:11 +0530

    Re-Update - next.config.ts file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         destination: process.env.NEXT_API_URL
-          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+          ? `${process.env.NEXT_API_URL}/api/:path*`
           : "http://localhost:5000/api/:path*",
       },
     ];
